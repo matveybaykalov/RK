d71339db5cbb9f72a3ce26a29bd4f665ea307fbc
matveybaykalov <70635348+matveybaykalov@users.noreply.github.com>
2020-09-30 20:42:18 +0300

    Update main.cpp
    Я поменял некоторые буквы при декодировании.

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main() {
     double s = 0, M[26] = {};
     string text;
 
-    for (int L=0; L<2; L++) {
+    for (int L=0; L<1; L++) {
         std::getline(cin, text);
         for (char i : text) {
             if (i >= 65 && i <= 90)
@@ -27,31 +27,31 @@ int main() {
     }
     for (char i : text){
         if (i=='D') cout << 'E'; else
-        if (i=='T') cout << 'T'; else
+        if (i=='T') cout << 'T'; else 
         if (i=='U') cout << 'A'; else
         if (i=='H') cout << 'O'; else
-        if (i=='L') cout << 'I'; else
-        if (i=='C') cout << 'L'; else
+        if (i=='L') cout << 'R'; else
+        if (i=='C') cout << 'I'; else
         if (i=='Y') cout << 'S'; else
         if (i=='K') cout << 'H'; else
         if (i=='X') cout << 'N'; else
         if (i=='O') cout << 'D'; else
-        if (i=='G') cout << 'R'; else
-        if (i=='M') cout << 'C'; else
-        if (i=='A') cout << 'U'; else
-        if (i=='Q') cout << 'M'; else
-        if (i=='E') cout << 'W'; else
-        if (i=='P') cout << 'F'; else
-        if (i=='N') cout << 'G'; else
-        if (i=='J') cout << 'Y'; else
-        if (i=='S') cout << 'P'; else
+        if (i=='G') cout << 'L'; else
+        if (i=='M') cout << 'M'; else
+        if (i=='A') cout << 'V'; else
+        if (i=='Q') cout << 'U'; else
+        if (i=='E') cout << 'G'; else
+        if (i=='P') cout << 'C'; else
+        if (i=='N') cout << 'F'; else
+        if (i=='J') cout << 'P'; else
+        if (i=='S') cout << 'Y'; else
         if (i=='Z') cout << 'B'; else
-        if (i=='B') cout << 'V'; else
+        if (i=='B') cout << 'W'; else
         if (i=='F') cout << 'K'; else
-        if (i=='I') cout << 'J'; else
-        if (i=='W') cout << 'X'; else
-        if (i=='V') cout << 'Q'; else
-        if (i=='R') cout << 'Z'; else cout <<static_cast<char>(i);
+        if (i=='I') cout << 'X'; else
+        if (i=='W') cout << 'J'; else
+        if (i=='V') cout << 'Z'; else
+        if (i=='R') cout << 'Q'; else cout <<static_cast<char>(i);
     }
 
     return 0;
